efb08a1cd6acbaba82ea890b251900fb98bc1d56
Klaus Dimde <klaus@dimde.info>
2024-04-08 21:55:45 +0200

    Added header

--- src/Modules/AjaxModule.php
+++ src/Modules/AjaxModule.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * @file AjaxModule.php
+ * Provides the basic class for all ajax modules
+ * Lang en
+ * Reviewstatus: 2024-04-08
+ * Localization:
+ * Documentation:
+ * Tests:
+ * Coverage: unknown
+ * PSR-State: complete
+ */
 
 namespace Sunhill\Framework\Modules;
 
