2d9485deaebecc12338f528962f35e5ad60d93d3
Klaus Dimde <klaus@dimde.info>
2024-04-08 21:12:50 +0200

    Added comments

--- src/Modules/FeatureModule.php
+++ src/Modules/FeatureModule.php
@@ -1,7 +1,25 @@
 <?php
+/**
+ * @file FeatureModule.php
+ * Provides the basic class for all feature modules
+ * Lang en
+ * Reviewstatus: 2024-04-08
+ * Localization:
+ * Documentation:
+ * Tests:
+ * Coverage: unknown
+ * PSR-State: complete
+ */
 
 namespace Sunhill\Framework\Modules;
 
+/**
+ * This class is a base class for a feature modules. A feature module is a collection of logical
+ * functions (like manipulating an item) and can be mounted into the module tree of the application.
+ * 
+ * @author klaus
+ *
+ */
 class FeatureModule extends AbstractModule
 {
     
