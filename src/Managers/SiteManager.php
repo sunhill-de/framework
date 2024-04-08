c364f07363eb725c87650d33077fbe5fcf839084
Klaus Dimde <klaus@dimde.info>
2024-04-08 20:42:39 +0200

    Added default views

---
+++ src/Managers/SiteManager.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Sunhill\Framework\Managers;
+
+use Sunhill\Framework\Modules\SkinModule;
+use Sunhill\Framework\Modules\AdminModule;
+
+class SiteManager 
+{
+    
+    public function installMainmodule($module)
+    {
+        
+    }
+    
+    public function installSkin(SkinModule $skin)
+    {
+        
+    }
+    
+    public function installAdminModule(AdminModule $module)
+    {
+        
+    }
+}
--- src/SunhillFrameworkServiceProvider.php
+++ src/SunhillFrameworkServiceProvider.php
@@ -5,6 +5,7 @@ namespace Sunhill\Framework;
 use Illuminate\Support\ServiceProvider;
 use Sunhill\Framework\Managers\PluginManager;
 use Sunhill\Framework\Facades\Plugins;
+use Sunhill\Framework\Managers\SiteManager;
 
 class SunhillFrameworkServiceProvider extends ServiceProvider
 {
@@ -12,10 +13,15 @@ class SunhillFrameworkServiceProvider extends ServiceProvider
     {
         $this->app->singleton(PluginManager::class, function () { return new PluginManager(); } );
         $this->app->alias(PluginManager::class,'plugins');
+        $this->app->singleton(SiteManager::class, function () { return new SiteManager(); } );
+        $this->app->alias(SiteManager::class,'site');
     }
     
     public function boot()
     {
         Plugins::setupPlugins();
+        
+        $this->loadViewsFrom(__DIR__.'/../resources/views','visual');
+        
     }
 }
