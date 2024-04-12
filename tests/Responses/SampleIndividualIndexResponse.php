e3470e2d2c5744cd59aafbfb890fb48d9240959f
Klaus Dimde <klaus@dimde.info>
2024-04-12 11:12:39 +0200

    Implemented module feature test

---
+++ tests/Responses/SampleIndividualIndexResponse.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Sunhhil\Framework\Tests\Responses;
+
+use Sunhill\Framework\Response\ViewResponses\TileViewResponse;
+
+class SampleIndividualIndexResponse extends TileViewResponse
+{
+    
+}
