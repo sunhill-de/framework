01264a49b52c939b865df0ad756dcf39a3aeb108
Klaus Dimde <klaus@dimde.info>
2024-04-28 19:50:36 +0200

    Implemented stub functions

--- src/Crud/AbstractCrudEngine.php
+++ src/Crud/AbstractCrudEngine.php
@@ -5,4 +5,31 @@ namespace Sunhill\Framework\Crud;
 class AbstractCrudEngine
 {
     
+    public function getElementCount(?string $filter): int
+    {        
+    }
+    
+    public function getColumns(): array
+    {       
+    }
+    
+    public function getColumnTitle(string $column): string
+    {
+        
+    }
+    
+    public function getListEntries(int $offset, int $limit, ?string $order, ?string $filter): array
+    {
+        
+    }
+    
+    public function isSortable(string $column): bool
+    {
+        
+    }
+    
+    public function getFilters(): array
+    {
+        
+    }
 }
