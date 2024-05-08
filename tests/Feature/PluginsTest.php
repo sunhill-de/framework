cd0caed1078548d7f5716d0012d19c24df77422e
Klaus Dimde <klaus@dimde.info>
2024-05-08 06:48:04 +0200

    Implemented plugin feature test

--- tests/Feature/PluginsTest.php
+++ tests/Feature/PluginsTest.php
@@ -3,13 +3,15 @@
 use Sunhill\Framework\Tests\TestCase;
 use Illuminate\Support\Facades\Config;
 use Sunhill\Framework\Managers\PluginManager;
+use Sunhill\Framework\Facades\Plugins;
 
 uses(\Sunhill\Framework\Tests\TestCase::class);
 
 
 test('boot', function () {
     Config::set('plugin_dir',dirname(__FILE__).'/../Plugins');
-    $manager = new PluginManager();
-    $results = $manager->setupPlugins();
-    expect($results[0]['data'])->toEqual('Boot:ModuleAModuleB');
+    Plugins::setKnownPlugins(['PluginA'=>'1.0.0']);
+    Plugins::setupPlugins();
+    
+    expect(Plugins::getPlugin('PluginA')->data)->toEqual('Boot:ModuleAModuleB');
 });
--- tests/Plugins/PluginA/PluginA.php
+++ tests/Plugins/PluginA/PluginA.php
@@ -8,10 +8,22 @@ require(dirname(__FILE__).'/src/ModuleB.php');
 class PluginA extends Plugin
 {
     
+    public $data = '';
+    
+    protected $version = '1.0.0';
+    
+    protected $name = 'PluginA';
+    
+    protected $dependencies = ['PluginB'];
+    
+    protected $author = 'John Doe';
+    
     public function boot()
     {
         $moduleA = new ModuleA();
         $moduleB = new ModuleB();
-        return 'Boot:'.$moduleA->doSomething().$moduleB->doSomething();
+        $this->data = 'Boot:'.$moduleA->doSomething().$moduleB->doSomething();
     }
+    
+    
 }
